fa3705074ecffe7ca020f6fc99c1e5783f41bdd5
Annie <annenark@gmail.com>
2025-06-04 14:45:51 -0500

    Added functionality and made some minor design changes

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,20 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
     var body: some View {
+        
         VStack{
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
-            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.black)
+                .padding(60)
+            TextField("Type your name here...", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
             Button("Submit Name") {
-                            
+                if name != "" {
+                    print(name)
+                    textTitle = "Welcome,  \(name)!"
+                }
+                else {
+                    textTitle = "Please enter a name"
+                }
             }
+            .padding(20)
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
+            .foregroundColor(Color.white)
+            .fontWeight(.black)
 
         }
         .padding()
@@ -31,3 +47,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
